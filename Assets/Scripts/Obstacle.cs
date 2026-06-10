2e699984b1617f0da55e8baec5311567aff9cb2a
Renanmc132 <renanmc100@gmail.com>
2026-06-10 19:55:47 -0300

    feat : Adciona randomização de tamanho do obstaculo

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+
+    private float minSize = 0.5f;
+    private float maxSize = 0.5f;
+    private Rigidbody2D _rb;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float size = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(size, size, 1);
+
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.AddForce(Vector2.right * 100);
     }
 
     // Update is called once per frame
