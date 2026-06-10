b5fba443e3d4b443aa19926688bac4764fb43167
Renanmc132 <renanmc100@gmail.com>
2026-06-10 20:24:59 -0300

    feat : Adciona movimentação do Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,56 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private Rigidbody2D _rb;
+    private float powerForce = 4f;
+    private float maxSpeed = 5f;
+
+    public GameObject rocketFlame;
+
+
+    void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
+
+
     void Update()
     {
-        
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+
+            Vector2 direction = (mousePos - transform.position).normalized;
+            transform.up = direction;
+
+            _rb.AddForce(powerForce * direction);
+        }
+
+        if(_rb.linearVelocity.magnitude > maxSpeed)
+        {
+            _rb.linearVelocity = _rb.linearVelocity.normalized * maxSpeed;
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            rocketFlame.SetActive(true);
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            rocketFlame.SetActive(false);
+        }
+
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
 }
